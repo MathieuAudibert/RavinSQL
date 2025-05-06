eab5a77c82c4dcba91183992ceb49e5a80699bb0
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-05-06 20:07:34 +0200

    ✨ feat:req

--- src/requetes_ravin.sql
+++ src/requetes_ravin.sql
@@ -1,25 +1,22 @@
 SELECT u.pseudo, COUNT(ar.id_a) AS act FROM Utilisateur AS u JOIN ActiviteReseau AS ar ON u.id_u = ar.id_utilisateur GROUP BY u.id_u ORDER BY act DESC LIMIT 10;
 -- recupere le TOP 10 des utilisateurs les plus actifs sur les reseaux V
 
-SELECT est_abonne, AVG(match_count) FROM (
-    SELECT u.id_u, u.est_abonne, COUNT(*) AS match_count
+SELECT est_abonne, AVG(match) FROM (
+    SELECT u.id_u, u.est_abonne, COUNT(*) AS match
     FROM Utilisateur AS u
     JOIN Match AS m ON u.id_u = m.id_utilisateur1 OR u.id_u = m.id_utilisateur2
     GROUP BY u.id_u, u.est_abonne
 ) AS q2 WHERE q2.est_abonne = TRUE GROUP BY q2.est_abonne;
 -- recupere le nombre moyen de match des utilisateurs abonnes  V
 
-SELECT u.est_abonne, AVG(match_count) AS average_matches
+SELECT est_abonne, AVG(match) AS moyenne
 FROM (
-    SELECT u.id_u, u.est_abonne, COUNT(*) AS match_count
+    SELECT u.id_u, u.est_abonne, COUNT(*) AS match
     FROM Utilisateur AS u
     JOIN Match AS m ON u.id_u = m.id_utilisateur1 OR u.id_u = m.id_utilisateur2
     GROUP BY u.id_u, u.est_abonne
-) AS subquery GROUP BY u.est_abonne;
+) GROUP BY est_abonne ORDER BY moyenne DESC;
 -- comparer le nombre de match des utilisateur abonnés et pas abonnés V
-
-SELECT * FROM Utilisateur;
--- recuperer les evenements les plus hype X
 
 SELECT u.pays, COUNT(ar.id_a) AS act FROM Utilisateur AS u JOIN ActiviteReseau AS ar ON u.id_u = ar.id_utilisateur 
 GROUP BY u.pays ORDER BY act DESC;
@@ -38,14 +35,12 @@ SELECT * FROM Utilisateur;
 SELECT * FROM Utilisateur;
 -- recuperer une correlation entre les utilisateurs qui 
 
-SELECT u1.pseudo AS utilisateur1, u2.pseudo AS utilisateur2, COUNT(DISTINCT t1.id_motcle) AS mc_commun 
-FROM Tag t1 JOIN Tag t2 ON t1.id_motcle = t2.id_motcle AND t1.id_utilisateur < t2.id_utilisateur
-JOIN Utilisateur AS u1 ON t1.id_utilisateur = u1.id_u
-JOIN Utilisateur AS u2 ON t2.id_utilisateur = u2.id_u
-GROUP BY u1.id_u, u2.id_u
-HAVING COUNT(DISTINCT t1.id_motcle) > 3
-ORDER BY mc_commun DESC;
--- utilisateurs avec des centres d'interet similaires V/X
+SELECT u1.pseudo AS utilisateur1, u2.pseudo AS utilisateur2
+FROM Utilisateur u1
+JOIN Utilisateur u2 ON u1.id_u <> u2.id_u
+JOIN Interet i1 ON u1.id_u = i1.id_utilisateur
+JOIN Interet i2 ON u2.id_u = i2.id_utilisateur AND i1.id_evenement = i2.id_evenement;
+-- recuperer les utilisateurs qui ont des interets communs V
 
 SELECT (COUNT(DISTINCT m.id_utilisateur1, m.id_utilisateur2)::FLOAT / COUNT(DISTINCT l.id_utilisateur1, l.id_utilisateur2)) * 100 AS taux
 FROM Likes AS l
@@ -67,3 +62,92 @@ SELECT EXTRACT(MONTH FROM e.date) as mois, COUNT(e.id_e) as evenements FROM Even
 JOIN Lieu AS l ON e.lieu = l.id_l GROUP BY l.type ORDER BY mois DESC;
 -- les type d'evenements les plus recurents au cours de l'année V
 
+SELECT u.pseudo
+FROM Utilisateur u
+WHERE (SELECT COUNT(*) FROM Participe p WHERE p.id_utilisateur = u.id_u) > (SELECT AVG(participations) FROM (SELECT COUNT(*) AS participations FROM Participe GROUP BY id_utilisateur) AS subquery);
+-- recuperer les utilisateurs qui participent plus que la moyenne des utilisateurs V
+
+SELECT u.pseudo
+FROM Utilisateur u
+WHERE NOT EXISTS (SELECT 1 FROM Evenement e WHERE NOT EXISTS (SELECT 1 FROM Participe p WHERE p.id_utilisateur = u.id_u AND p.id_evenement = e.id_e));
+--  recuperer les utilisateurs qui participent plus que la moyenne des utilisateurs sous requetes corrélées V
+
+SELECT u.pseudo
+FROM Utilisateur u
+JOIN Participe p ON u.id_u = p.id_utilisateur
+GROUP BY u.id_u
+HAVING COUNT(DISTINCT p.id_evenement) = (SELECT COUNT(*) FROM Evenement);
+--  recuperer les utilisateurs qui participent plus que la moyenne des utilisateurs avec aggrégation V
+
+SELECT pseudo FROM Utilisateur WHERE poids > 70;
+-- recuperer les utilisateurs qui pèsent plus de 70kg V
+SELECT pseudo FROM Utilisateur WHERE poids IS NOT NULL AND poids > 70;
+-- recuperer les utilisateurs qui pèsent plus de 70kg avec gestion des nulls V
+
+WITH RECURSIVE disponible AS (
+    SELECT id_l, MIN(date) AS prochaine_date
+    FROM Evenement
+    GROUP BY id_l
+    UNION ALL
+    SELECT e.id_l, MIN(e.date)
+    FROM Evenement e
+    JOIN disponible d ON e.id_l = d.id_l AND e.date > d.prochaine_date
+    GROUP BY e.id_l
+)
+SELECT id_l, prochaine_date FROM disponible WHERE prochaine_date > NOW() ORDER BY prochaine_date LIMIT 1;
+-- recuperer le prochain evenement d'un lieu X
+
+SELECT pseudo, mot, RANK() OVER (PARTITION BY DATE_TRUNC('month', date) ORDER BY COUNT(*) DESC) AS rang
+FROM (
+    SELECT u.pseudo, m.mot, COUNT(*)
+    FROM Utilisateur u
+    JOIN Tag t ON u.id_u = t.id_utilisateur
+    JOIN MotCle m ON t.id_motcle = m.id_m
+    JOIN ActiviteReseau ar ON u.id_u = ar.id_utilisateur
+    WHERE ar.type = 'nope'
+    GROUP BY u.pseudo, m.mot, DATE_TRUNC('month', ar.date)
+) AS subquery
+WHERE rang <= 10;
+-- recuperer le top 10 des mots les plus tagués par mois X
+
+SELECT u.pseudo, COUNT(e.id_e) AS nombre_evenements
+FROM Utilisateur u
+JOIN Participe p ON u.id_u = p.id_utilisateur
+JOIN Evenement e ON p.id_evenement = e.id_e
+GROUP BY u.pseudo;
+-- recuperer le nombre d'evenements auxquels chaque utilisateur a participé V
+
+SELECT pseudo FROM Utilisateur
+WHERE id_u NOT IN (SELECT id_utilisateur FROM Participe);
+-- recuperer les utilisateurs qui n'ont pas participé à d'evenements V
+
+SELECT AVG(poids) AS poids_moyen FROM Utilisateur WHERE poids IS NOT NULL;
+-- recuperer le poids moyen des utilisateurs V
+
+SELECT * FROM Evenement WHERE date > NOW();
+-- recuperer les evenements à venir V
+
+SELECT u.pseudo, COUNT(*) AS likes
+FROM Utilisateur AS u
+JOIN Likes AS l ON u.id_u = l.id_utilisateur1
+GROUP BY u.pseudo ORDER BY likes DESC;
+-- recuperer le nombre de likes de chaque utilisateur V
+
+SELECT DISTINCT l.adresse
+FROM Lieu l
+JOIN Evenement e ON l.id_l = e.lieu;
+-- recuperer les adresses des lieux qui ont eu des evenements V
+
+SELECT u.pseudo
+FROM Utilisateur u
+JOIN Participe p ON u.id_u = p.id_utilisateur
+JOIN Evenement e ON p.id_evenement = e.id_e
+GROUP BY u.pseudo
+HAVING COUNT(DISTINCT e.lieu) > 1;
+-- recuperer les utilisateurs qui ont participé à des evenements dans plusieurs lieux V
+
+SELECT DATE_TRUNC('month', date_inscription) AS mois, COUNT(*) AS nouveaux_utilisateurs
+FROM Utilisateur
+GROUP BY mois
+ORDER BY mois;
+-- recuperer le nombre de nouveaux utilisateurs par mois X
